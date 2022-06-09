c5dbedbc8638b66049604fa2ea12da9c7fbd5c49
JATIN <jatin.dhar27@gmail.com>
2022-06-09 18:54:18 +0530

    Update settings.py

--- covid_test/settings.py
+++ covid_test/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
